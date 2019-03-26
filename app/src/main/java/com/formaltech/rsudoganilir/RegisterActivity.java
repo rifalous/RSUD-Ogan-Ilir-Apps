720ef43c5cec5a47515552aeba7e1d32ef4bdc97
Rivaldy Fauzan <rifalous@gmail.com>
2019-03-26 14:03:25 +0700

    Adding Connectivity Manager

--- app/src/main/java/com/formaltech/rsudoganilir/RegisterActivity.java
+++ app/src/main/java/com/formaltech/rsudoganilir/RegisterActivity.java
@@ -1,32 +1,47 @@
 package com.formaltech.rsudoganilir;
 
+import android.content.Context;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextView titlepage, subtitlepage;
-    Button btnrecon;
+    ImageView no_internet_iv;
+    TextView title_status, subtitle_status, masih_kosong;
+    Button btn_recon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        titlepage = findViewById(R.id.titlepage);
-        subtitlepage = findViewById(R.id.subtitlepage);
-        btnrecon = findViewById(R.id.btnrecon);
+        title_status = findViewById(R.id.title_status);
+        subtitle_status = findViewById(R.id.subtitle_status);
+        masih_kosong = findViewById(R.id.masih_kosong);
+        no_internet_iv = findViewById(R.id.no_internet_iv);
+        btn_recon = findViewById(R.id.btn_recon);
 
         Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
         Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
         Typeface Vidaloka = Typeface.createFromAsset(getAssets(), "fonts/Vidaloka.ttf");
 
-        titlepage.setTypeface(Vidaloka);
-        subtitlepage.setTypeface(MLight);
-        btnrecon.setTypeface(MMedium);
+        title_status.setTypeface(Vidaloka);
+        subtitle_status.setTypeface(MLight);
+        btn_recon.setTypeface(MMedium);
+
+        btn_recon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkNetworkConnectStatus();
+            }
+        });
 
         /*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -37,6 +52,31 @@ public class RegisterActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         */
+    }
+
+    private void checkNetworkConnectStatus() {
+        boolean wifiConnected;
+        boolean mobileConnected;
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
+        if (activeInfo != null && activeInfo.isConnected()) {
+
+            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+            mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+
+            if (wifiConnected || mobileConnected) {
+                title_status.setVisibility(View.GONE);
+                subtitle_status.setVisibility(View.GONE);
+                btn_recon.setVisibility(View.GONE);
+                masih_kosong.setVisibility(View.VISIBLE);
+            }
+        } else {
+            title_status.setVisibility(View.VISIBLE);
+            subtitle_status.setVisibility(View.VISIBLE);
+            btn_recon.setVisibility(View.VISIBLE);
+            masih_kosong.setVisibility(View.GONE);
+        }
     }
 
     /*
